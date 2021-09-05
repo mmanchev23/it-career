aa3a768ad97cdd3cb659cbf73207d2db9094c4bb
mmanchev23 <mmanchev2003@gmail.com>
2021-09-05 23:00:11 +0300

    Solved Problem from Module 9.

---
+++ Module 9/01. greedy algorithms/Solutions/Magnetic Tape/Magnetic Tape/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Magnetic_Tape
+{
+    class Program
+    {
+        public static void CalculateTapes(Tape[] tapes)
+        {
+            for (int i = 0; i < tapes.Length; i++)
+            {
+                tapes[i].Result = tapes[i].Length / tapes[i].Program;
+            }
+
+            tapes = tapes.OrderByDescending(tape => tape.Result).ToArray();
+
+            foreach(var tape in tapes)
+            {
+                Console.Write($"{tape.Probability} ");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("How many tapes are there? ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            string[] input;
+            Tape[] tapes = new Tape[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Tape #{i + 1}: ");
+                input = Console.ReadLine().Split(" ");
+                tapes[i] = new Tape(int.Parse(input[0]), int.Parse(input[1]), double.Parse(input[2]), 0);
+            }
+
+            CalculateTapes(tapes);
+        }
+    }
+}
+/*
+4
+1 1 0.3
+2 3 0.1
+3 2 0.5
+4 4 0.2
+
+1 3 4 2
+*/
---
+++ Module 9/01. greedy algorithms/Solutions/Magnetic Tape/Magnetic Tape/Tape.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Magnetic_Tape
+{
+    public class Tape
+    {
+        private int probability;
+        private int program;
+        private double length;
+        private double result;
+
+        public int Probability
+        {
+            get => this.probability;
+            set => this.probability = value;
+        }
+
+        public int Program
+        {
+            get => this.program;
+            set => this.program = value;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            set => this.length = value;
+        }
+
+        public double Result
+        {
+            get => this.result;
+            set => this.result = value;
+        }
+
+        public Tape(int probability, int program, double length, double result)
+        {
+            this.Probability = probability;
+            this.Program = program;
+            this.Length = length;
+            this.Result = result;
+        }
+    }
+}
