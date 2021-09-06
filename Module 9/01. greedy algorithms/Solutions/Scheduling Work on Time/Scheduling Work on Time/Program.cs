aa737411f9f8568c9b49749c720bca2c1f70671e
mmanchev23 <mmanchev2003@gmail.com>
2021-09-06 22:38:33 +0300

    Solved a problem from 'Greedy algorithms'.

---
+++ Module 9/01. greedy algorithms/Solutions/Scheduling Work on Time/Scheduling Work on Time/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace Scheduling_Work_on_Time
+{
+    class Program
+    {
+        public static void CalculateWork(Work[] works)
+        {
+            works = works.OrderByDescending(work => work.Prize).ThenBy(work => work.Deadline).ToArray();
+            int prize = 0;
+            int i, j;
+            i = 0;
+
+            Console.Write($"\n{works[0].Name} ");
+
+            for (j = 1; j < works.Length; j++)
+            {
+                if(works[j].Deadline > works[i].Deadline)
+                {
+                    prize += works[j].Prize;
+                    Console.Write($"{works[j].Name} ");
+                    i = j;
+                }
+            }
+            Console.WriteLine($"\n{prize + works[0].Prize}");
+        }
+
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string[] work;
+            Work[] works = new Work[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                work = Console.ReadLine().Split();
+                works[i] = new Work(work[0], int.Parse(work[1]), int.Parse(work[2]));
+            }
+
+            CalculateWork(works);
+        }
+    }
+}
+
+/*
+5
+j1 2 60
+j2 1 100
+j3 3 20
+j4 2 40
+j5 1 20
+
+j2 j1 j3
+180
+*/
---
+++ Module 9/01. greedy algorithms/Solutions/Scheduling Work on Time/Scheduling Work on Time/Work.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scheduling_Work_on_Time
+{
+    class Work
+    {
+        private string name;
+        private int deadline;
+        private int prize;
+
+        public string Name
+        {
+            get => this.name;
+            set => this.name = value;
+        }
+
+        public int Deadline
+        {
+            get => this.deadline;
+            set => this.deadline = value;
+        }
+
+        public int Prize 
+        {
+            get => this.prize;
+            set => this.prize = value;
+        }
+
+        public Work(string name, int deadline, int prize)
+        {
+            this.Name = name;
+            this.Deadline = deadline;
+            this.Prize = prize;
+        }
+    }
+}
