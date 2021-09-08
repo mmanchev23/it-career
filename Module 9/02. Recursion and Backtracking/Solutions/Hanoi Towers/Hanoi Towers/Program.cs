959e85a8686d0b23aabba13baffc2a84c4cc9014
mmanchev23 <mmanchev2003@gmail.com>
2021-09-08 18:37:12 +0300

    Solved 'Hanoi Towers' Problem.

---
+++ Module 9/02. Recursion and Backtracking/Solutions/Hanoi Towers/Hanoi Towers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hanoi_Towers
+{
+    class Program
+    {
+        public static void solveTowers(int total_disks, char start, char end, char temp)
+        {
+            if (total_disks > 0)
+            {
+                solveTowers(total_disks - 1, start, temp, end);
+                Console.WriteLine("Move disk from " + start + ' ' + end);
+                solveTowers(total_disks - 1, temp, end, start);
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            char start = 'A';
+            char end = 'C';
+            char temp = 'B';
+            int total_disks = 3;
+
+            solveTowers(total_disks, start, end, temp);
+        }
+    }
+}
---
+++ Module 9/02. Recursion and Backtracking/Solutions/The Horse Ride/The Horse Ride/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace The_Horse_Ride
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
