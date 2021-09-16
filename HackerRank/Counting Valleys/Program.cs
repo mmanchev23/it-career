0ad138c51b70523411e42efd1a4aff26e7545749
mmanchev23 <mmanchev2003@gmail.com>
2021-09-16 22:52:07 +0300

    Solved 'Counting Valleys' from HackerRank in CSharp.

---
+++ HackerRank/Counting Valleys/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Counting_Valleys
+{
+    class Program
+    {
+        public static int countingValleys(int steps, string path)
+        {
+            int current_level = 0;
+            int valleys = 0;
+
+            for (int i = 0; i < steps; i++)
+            {
+                if (path[i] == 'U')
+                {
+                    current_level++;
+                    if (current_level == 0)
+                    {
+                        valleys++;
+                    }
+                }
+                else
+                {
+                    current_level--;
+                }
+            }
+
+            return valleys;
+        }
+
+        static void Main(string[] args)
+        {
+            int steps = int.Parse(Console.ReadLine());
+            string path = Console.ReadLine();
+
+            countingValleys(steps, path);
+        }
+    }
+}
