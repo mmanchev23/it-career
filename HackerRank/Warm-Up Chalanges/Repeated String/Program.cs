1dbed9bdc2d5e2703561531af0422adef91066c1
mmanchev23 <mmanchev2003@gmail.com>
2021-09-16 23:12:10 +0300

    Solved 'Repeated String' from HackerRank with CSharp.

---
+++ HackerRank/Warm-Up Chalanges/Repeated String/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Repeated_String
+{
+    class Program
+    {
+        public static long repeatedString(string s, long n)
+        {
+            long repeated = n / s.Length;
+            long left = n - (s.Length * repeated);
+            int extra = 0;
+            int count = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    ++count;
+                }
+            }
+
+            for (int i = 0; i < left; i++)
+            {
+                if (s[i] == 'a')
+                {
+                    ++extra;
+                }
+            }
+
+            return (repeated * count) + extra;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            long n = long.Parse(Console.ReadLine());
+
+            repeatedString(s, n);
+        }
+    }
+}
