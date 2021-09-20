656bbee34b07d45bff306ce01fe7f255e9580e9b
mmanchev23 <mmanchev2003@gmail.com>
2021-09-20 18:17:24 +0300

    Added Linked List Implementation!

---
+++ Data Structures/Linked List/Node.cs
@@ -0,0 +1,26 @@
+﻿namespace Linked_List
+{
+    public class Node
+    {
+        private int data;
+        private Node next;
+
+        public int Data
+        {
+            get => this.data;
+            set => this.data = value;
+        }
+
+        public Node Next
+        {
+            get => this.next;
+            set => this.next = value;
+        }
+
+        public Node(int data)
+        {
+            this.Data = data;
+            this.Next = null;
+        }
+    }
+}
