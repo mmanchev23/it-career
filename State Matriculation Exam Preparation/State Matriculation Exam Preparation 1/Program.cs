d83d8fb547494bde7ac70bdebe9e2edc5c86309a
Martin Manchev <72170828+mmanchev23@users.noreply.github.com>
2021-09-30 08:19:59 +0300

    Update Program.cs

--- State Matriculation Exam Preparation/State Matriculation Exam Preparation 1/Program.cs
+++ State Matriculation Exam Preparation/State Matriculation Exam Preparation 1/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
-using System.IO;
+using System;
 using System.Linq;
+using System.IO;
+using System.Collections.Generic;
 
 namespace State_Matriculation_Exam_Preparation_1
 {
@@ -8,21 +9,22 @@ namespace State_Matriculation_Exam_Preparation_1
     {
         static void Main(string[] args)
         {
-            StreamReader stream_reader = new StreamReader(@"./data.txt");
+            StreamReader stream_reader = new StreamReader(@"data.txt");
+            Stack<Car> results = new Stack<Car>();
 
             while (!stream_reader.EndOfStream)
             {
                 int type_of_vehicle = int.Parse(stream_reader.ReadLine());
                 string brand = stream_reader.ReadLine();
-                int[] properties = stream_reader.ReadLine().Split().Select(int.Parse).ToArray();
+                double[] properties = stream_reader.ReadLine().Split().Select(double.Parse).ToArray();
 
                 if (type_of_vehicle == 1)
                 {
-                    Console.WriteLine(new Car(brand, properties[0], properties[1], properties[2]).ToString());
+                    results.Push(new Car(brand, (int)(properties[0]), (int)(properties[1]), properties[2]));
                 }
                 else if (type_of_vehicle == 2)
                 {
-                    Console.WriteLine(new Truck(brand, properties[0], properties[1], properties[2], properties[3]).ToString());
+                    results.Push(new Truck(brand, (int)(properties[0]), (int)(properties[1]), properties[2], (int)(properties[3])));
                 }
                 else
                 {
@@ -30,6 +32,7 @@ namespace State_Matriculation_Exam_Preparation_1
                 }
             }
 
+            Console.WriteLine(string.Join(Environment.NewLine, results));
             stream_reader.Close();
         }
     }
