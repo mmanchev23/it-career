25dfe7b7ac709b919859d0386825953628bdbd1f
mmanchev23 <mmanchev2003@gmai.com>
2021-10-17 17:28:02 +0300

    Added Graphs Implementation 1/2.

---
+++ IT Career/Module 9/07. graphs and graph algorithms/Solutions/Graphs/Graphs/GraphMatrix.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graphs
+{
+    class GraphMatrix
+    {
+        private int[,] vertices;
+
+        public int[,] Vertices
+        {
+            get => this.vertices;
+            set => this.vertices = value;
+        }
+
+        public GraphMatrix(int[,] vertices)
+        {
+            this.vertices = vertices;
+        }
+
+        public void AddEdge(int i, int j, int weight)
+        {
+            vertices[i, j] = weight;
+        }
+
+        public void AddEdge(int i, int j)
+        {
+            vertices[i, j] = 1;
+        }
+
+        public void RemoveEdge(int i, int j)
+        {
+            vertices[i, j] = 0;
+        }
+
+        public bool HasEdge(int i, int j)
+        {
+            return vertices[i, j] != 0;
+        }
+
+        public IList<int> GetSuccessors(int i)
+        {
+            IList<int> successors = new List<int>();
+
+            for (int j = 0; j < vertices.GetLength(1); j++)
+            {
+                if (vertices[i, j] != 0)
+                {
+                    successors.Add(j);
+                }
+            }
+            return successors;
+        }
+
+        public void PrintBFS(int startV)
+        {
+            if (startV >= this.vertices.GetLength(0))
+            {
+                throw new ArgumentException("No such vertice!");
+            }
+
+            IList<int> visited = new List<int>();
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(startV);
+            visited.Add(startV);
+
+            while (queue.Count != 0)
+            {
+                int current = queue.Dequeue();
+                Console.Write(current + " ");
+
+                foreach (int child in this.GetSuccessors(current))
+                {
+                    if (!visited.Contains(child))
+                    {
+                        visited.Add(child);
+                        queue.Enqueue(child);
+                    }
+                }
+            }
+        }
+
+        public void PrintDFS(int startV)
+        {
+            if (startV >= this.vertices.GetLength(0))
+            {
+                throw new ArgumentException("No such vertice!");
+            }
+
+            IList<int> visited = new List<int>();
+            Stack<int> stack = new Stack<int>();
+
+            stack.Push(startV);
+            visited.Add(startV);
+
+            while (stack.Count != 0)
+            {
+                int current = stack.Pop();
+                Console.Write(current + " ");
+
+                foreach (int child in this.GetSuccessors(current))
+                {
+                    if (!visited.Contains(child))
+                    {
+                        visited.Add(child);
+                        stack.Push(child);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ IT Career/Module 9/07. graphs and graph algorithms/Solutions/Graphs/Graphs/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Graphs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter node to start: ");
+            int start = int.Parse(Console.ReadLine());
+
+            int[,] graph = new int[,] {
+                { 0, 1, 1, 1, 1 },
+                { 1, 0, 0, 0, 1 },
+                { 1, 0, 0, 1, 1 },
+                { 1, 0, 1, 0, 0 },
+                { 1, 1, 1, 0, 0 },
+            };
+
+            GraphMatrix gm = new GraphMatrix(graph);
+
+            try
+            {
+                Console.Write($"BFS from node {start}: ");
+                gm.PrintBFS(start);
+                Console.WriteLine();
+
+                Console.Write($"DFS drom node {start}: ");
+                gm.PrintDFS(start);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
