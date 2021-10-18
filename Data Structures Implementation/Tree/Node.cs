ee523af0ed0453321948eff5fdc569b9c48bf937
mmanchev23 <mmanchev2003@gmai.com>
2021-10-18 23:24:10 +0300

    Added Tree Implementation.

---
+++ Data Structures Implementation/Tree/Node.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tree
+{
+    class Node<T>
+    {
+        private T data;
+        private IList<Node<T>> children;
+        private bool has_parent;
+
+        public T Data
+        {
+            get => this.data;
+            set => this.data = value != null ? value : throw new ArgumentNullException();
+        }
+
+        public IList<Node<T>> Children
+        {
+            get => this.children;
+            set => this.children = value != null ? value : throw new ArgumentNullException();
+        }
+
+        public bool Has_Parent
+        {
+            get => this.has_parent;
+            set => this.has_parent = value;
+        }
+
+        public int ChildrenCount
+        {
+            get => this.Children.Count;
+        }
+
+        public Node(T data)
+        {
+            this.Data = data;
+            this.Children = new List<Node<T>>();
+        }
+
+        public void AddChild(Node<T> node)
+        {
+            if (node == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (node.Has_Parent)
+            {
+                throw new ArgumentException();
+            }
+
+            node.Has_Parent = true;
+            this.Children.Add(node);
+        }
+
+        public Node<T> GetChild(int index)
+        {
+            return this.Children[index];
+        }
+    }
+}
---
+++ IT Career/Module 9/03. combinatorics/Solutions/01. Permutations/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _01._Permutations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+
+        public static long Factorial(long n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+    }
+}
